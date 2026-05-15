d094dd242e9634d5c34c6e2de1bdfec1b821531e
ontheway0335 <wang07955837788@gmail.com>
2026-05-15 17:56:53 +0100

    Update index.go

--- api/index.go
+++ api/index.go
@@ -2,19 +2,23 @@ package handler
 
 import (
 	"net/http"
-	"one-api/common"     // 必须和 go.mod 里的 module 名称一致
+	"one-api/common"
 	"one-api/model"
 	"one-api/router"
 	"github.com/gin-gonic/gin"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	// 初始化配置
 	common.SetupLogger()
 	common.Init()
 	model.InitDB()
 	
+	// 设置路由
 	gin.SetMode(gin.ReleaseMode)
 	server := gin.Default()
 	router.SetRouter(server)
+	
+	// 响应请求
 	server.ServeHTTP(w, r)
 }
