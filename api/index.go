23ecaf1ddea910f0053e2d549f646b425afe11ae
ontheway0335 <wang07955837788@gmail.com>
2026-05-15 17:58:50 +0100

    Update index.go

--- api/index.go
+++ api/index.go
@@ -9,16 +9,16 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// 初始化配置
+	// 1. 初始化程序
 	common.SetupLogger()
 	common.Init()
 	model.InitDB()
 	
-	// 设置路由
+	// 2. 设置路由
 	gin.SetMode(gin.ReleaseMode)
 	server := gin.Default()
 	router.SetRouter(server)
 	
-	// 响应请求
+	// 3. 将 Vercel 请求转接给 Gin 框架
 	server.ServeHTTP(w, r)
 }
